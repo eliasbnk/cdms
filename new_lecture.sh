69bcdc740ee7571614e2e97fff2a6c17043980bc
Elias Babenko <63197354+eliasbnk@users.noreply.github.com>
2024-09-13 03:17:33 -0700

    Update new_lecture.sh

--- new_lecture.sh
+++ new_lecture.sh
@@ -1,23 +1,23 @@
 #!/bin/zsh
 
-# Define color codes
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
-YELLOW='\033[1;33m' # Bright yellow for numbers
-NC='\033[0m' # No Color to reset
+YELLOW='\033[1;33m' 
+NC='\033[0m'
 
-# Define the base directory
+
 base_dir=~/College
 
-# Get today's date in YYYYMMDD format
+
 current_date=$(date +'%Y%m%d')
 
-# Initialize an array to store class names
-class_names=()
-index=1 # Zsh arrays are 1-indexed
 
-# Populate class_names with class names without the full path, excluding any 'Assignments' directory
+class_names=()
+index=1 
+
+
 for class_dir in "$base_dir"/*; do
     if [ -d "$class_dir" ] && [ "$(basename "$class_dir")" != "Assignments" ]; then
         class_names[index]="$(basename "$class_dir")"
@@ -25,7 +25,7 @@ for class_dir in "$base_dir"/*; do
     fi
 done
 
-# Display class names to choose from and ask for user input
+
 echo -e "${CYAN}Choose a class:${NC}"
 for i in {1..$#class_names}; do
     echo -e "${YELLOW}${i})${NC} ${class_names[i]}"
@@ -34,28 +34,28 @@ done
 echo -en "${CYAN}Enter the number of your choice: ${NC}"
 read choice_num
 
-# Validate the input and proceed if valid
+
 if (( choice_num >= 1 && choice_num <= $#class_names )); then
     selected_class="${class_names[choice_num]}"
 
-    # Create a new lecture folder with today's date
+    
     new_dir="$base_dir/$selected_class/Lectures/$current_date"
     
-    # Check if the folder already exists and append a counter if needed
+    
     counter=2
     while [ -d "$new_dir" ]; do
         new_dir="$base_dir/$selected_class/Lectures/${current_date}_#${counter}"
         ((counter++))
     done
     
-    # Create separate directories for Notes, Slides, and Videos
+    
     mkdir -p "$new_dir/Notes"
     mkdir -p "$new_dir/Slides"
     mkdir -p "$new_dir/Videos"
     
     echo -e "${GREEN}New lecture folder created: $new_dir${NC}"
 
-    # Ask the user if they want to change directory
+    
     echo -en "${CYAN}Do you want to change to this folder? (yes/no): ${NC}"
     read choice
     case "$choice" in
