a5644d0c95e2e5059e2d94ec598ef06c3c2f8739
Elias Babenko <63197354+eliasbnk@users.noreply.github.com>
2024-09-13 03:18:40 -0700

    Update setup_college.sh

--- setup_college.sh
+++ setup_college.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# Define color codes
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
-# Define the base directory
+
 base_dir=~/College
 
-# Check if the College directory exists
+
 if [ -d "$base_dir" ]; then
     echo -e "${CYAN}Checking for existing classes in the College directory...${NC}"
-    # Check if the directory contains any subdirectories
+    
     if [ "$(ls -A $base_dir)" ]; then
         echo -e "${RED}Classes already exist in the College directory. Exiting...${NC}"
         exit 1
     fi
 else
-    # Create the College directory if it doesn't exist
+    
     echo -e "${GREEN}Creating the College directory...${NC}"
     mkdir -p "$base_dir"
 fi
 
-# Prompt user to enter class names
+
 echo -e "${CYAN}Enter class name (after each class name press ENTER) or 0 to exit:${NC}"
 while :; do
     echo -n "> "
@@ -33,7 +33,7 @@ while :; do
         break
     fi
     class_dir="$base_dir/$class_name"
-    # Check if the class directory already exists
+    
     if [ -d "$class_dir" ]; then
         echo -e "${YELLOW}Warning: A folder named $class_name already exists. Override? (yes/no):${NC}"
         read override
